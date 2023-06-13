9cbfba08a94a57b06f0eb0c91b9f4d056c83ac3c
bao truong <truongquangbaolut0901@gmail.com>
2023-06-13 23:19:35 +0700

    baotq-140623

--- src/pages/ResetPassword.js
+++ src/pages/ResetPassword.js
@@ -41,37 +41,38 @@ const ResetPassword = () => {
     })
     return (
         <div className='reset-password'>
-            <div className='header'>
+            {/* <div className='header'>
                 <div className='container-xxl'>
                     <div className='row'>
                         <div className='col-12'>
                             <Breadcrumb className='container new-header d-flex justify-content-center mt-2 mb-3'>
                                 <NavLink to="/" className="breadcrumb-item">Home</NavLink>
-                                <NavLink to="/forgot-password" className="breadcrumb-item">Forgot Password</NavLink>
                                 <Breadcrumb.Item active>Reset Password</Breadcrumb.Item>
                             </Breadcrumb>
                         </div>
                     </div>
                 </div>
-            </div>
-            <div className='main py-5'>
-                <div className='container-xxl'>
-                    <form onSubmit={formik.handleSubmit} className='col-3'>
-                        <h4 className='mb-1'>Confirm New Password</h4>
-                        <Input
-                            placeholder="New Password"
-                            name="password"
-                            type="password"
-                            // label="Email"
-                            value={formik.values.password}
-                            onChange={formik.handleChange("password")}
-                            errorMessage={formik.errors.password}
-                        />
-                        <button type="submit" disabled={isLoading} className='btn btn-submit'>
-                            {isLoading === true && <AiOutlineLoading3Quarters className='loading-icon' />}
-                            Submit
-                        </button>
-                    </form>
+            </div> */}
+
+            <div className='container-xxl'>
+                <div className='main py-5'>
+                    <div className='row'>
+                        <form onSubmit={formik.handleSubmit} className='col-12 col-sm-4 col-md-5'>
+                            <h4 className='mb-1'>Confirm New Password</h4>
+                            <Input
+                                placeholder="New Password"
+                                name="password"
+                                type="password"
+                                value={formik.values.password}
+                                onChange={formik.handleChange("password")}
+                                errorMessage={formik.errors.password}
+                            />
+                            <button type="submit" disabled={isLoading} className='btn btn-submit'>
+                                {isLoading === true && <AiOutlineLoading3Quarters className='loading-icon' />}
+                                Submit
+                            </button>
+                        </form>
+                    </div>
                 </div>
             </div>
 
